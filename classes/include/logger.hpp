28c6b6604ff8f530e35b3bec2f49805085578f7e
enzo-mattio <enzo.mattio@laplateforme.io>
2024-09-02 10:11:03 +0200

    [ADD] Logger + file to store the logs

---
+++ classes/include/logger.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <chrono>
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+
+class EventMessage {
+public:
+    int level;
+    std::string message;
+    std::chrono::system_clock::time_point timeEvent;
+};
+
+class AppLogs {
+private:
+    static std::unique_ptr<AppLogs> _instance;
+    std::vector<EventMessage> messages;
+
+    AppLogs() = default;
+
+public:
+    std::string path = "";
+    std::string fileName = "logs";
+    std::string fileExtension = "txt";
+    bool filenamedWithTimestamp = false;
+    bool display = false;
+    int currentLevel = 1;
+
+    static AppLogs& Instance();
+    void EventLog(int lv, const std::string& m);
+    int GetNumberForLevel(int lv);
+    std::string GenerateFile();   
+};
